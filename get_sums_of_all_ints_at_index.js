172f00dbbdb0d77aa2aae389df133de41b922400
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-16 10:33:05 -0500

    labwork saturday class, return a list fo sums from integers

---
+++ get_sums_of_all_ints_at_index.js
@@ -0,0 +1,13 @@
+var array = [1, 7, 3, 4];
+var sum = [];
+var elementOne = ([1]+ [2] + [3]);
+var elementTwo = ([0]+ [2] + [3]);
+var elementThree = ([0] + [1] + [3]);
+var elementFour = ([0] + [1] + [2]);
+
+
+
+
+for( var = 0; i < array.length; i++){
+    sum = 
+}
